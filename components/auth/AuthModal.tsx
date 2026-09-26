6e3ccc277323ac114c3e7775b0b8bb8c9e84d313
yernur-sh <ernurshahar0@gmail.com>
2026-09-26 20:34:57 +0500

    Class

--- components/auth/AuthModal.tsx
+++ components/auth/AuthModal.tsx
@@ -62,7 +62,7 @@ export default function AuthModal() {
                 className="input"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
-                placeholder="Арман Сейітов"
+                placeholder=""
                 required
               />
             </div>
